8af799bf6bb0f4f31a1e7a03dc843b31ac737331
Per I. H <you@example.com>
2020-11-04 08:37:02 +0100

    Select version

--- public/index.js
+++ public/index.js
@@ -6,6 +6,17 @@ async function get(url) {
     return await respons.json();
 }
 
+async function post(url, objekt) {
+    const respons = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(objekt),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (respons.status !== 201) // Created
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
 async function getText(url) {
     const respons = await fetch(url);
     if (respons.status !== 200) // OK
@@ -21,37 +32,58 @@ async function generateJokesTable(jokes) {
 
 async function main() {
     try {
-        let jokes = await get('joke/api/jokes');
-        let div = document.getElementById('div1');
+        let jokes = await get('/joke/api/jokes');
+        let div = document.getElementById('div1')
         div.innerHTML = await generateJokesTable(jokes);
-
     } catch (e) {
         console.log(e.name + ": " + e.message);
     }
 }
-
-async function post(url, objekt) {
-    const respons = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(objekt),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    if (respons.status !== 201) // Created
-        throw new Error(respons.status);
-    return await respons.json();
-}
-
-
 main();
 
-let opretButton = document.getElementById('opretButton');
+let opretButton = document.getElementById('opretButton')
+
 opretButton.onclick = async () => {
     try {
-        await post("/joke/api/jokes/", { setup: "setup.value", punchline: "punchline.value" })
+        await post("/joke/api/jokes", { setup: 'setup.value', punchline: 'punchline.value' });
     } catch (e) {
+    }
+}
+
+let selectSite = document.getElementById('selectSite')
+
+
+async function getSites() {
+    try {
+        let result = await get('https://krdo-joke-registry.herokuapp.com/api/services');
+        createSelect(result)
+    }
+    catch (e) {
         console.log(e);
+    }
+}
+
+function createSelect(result) {
+    let siteArray = []
+    console.log(result)
+    for (let i = 0; i < result.length; i++) {
+        siteArray.push(result[i].address)
+        console.log(siteArray[i])
+        let option = document.createElement('option')
+        option.text = siteArray[i]
+        selectSite.add(option, i)
     }
 
 }
+getSites()
 
+async function generateSelect() {
+    try {
+        let sites = await get('https://krdo-joke-registry.herokuapp.com/api/services')
+        return sites
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
 
